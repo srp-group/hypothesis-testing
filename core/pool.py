7263844c14b65461ad7ba143213f820face1126c
Brillianda Sheravina <shera.assa@gmail.com>
2024-05-15 17:48:34 +0200

    edit the method run to test run

--- core/pool.py
+++ core/pool.py
@@ -23,18 +23,31 @@ class Pool():
         # setting the indecies
         self.set_seed()
         self.idx_abs = np.arange(len(self.dataset)) # absolute indecies
-        # setting the labeled indecies
-        initial_lb_size = float(self.dataset_config['labeled_share']) * len(self.dataset) # initial labeled size
-        self.set_seed()
-        self.idx_label = np.random.choice(self.idx_abs, size=math.floor(initial_lb_size), replace=False) # original labeled indecies
+
         # static hold-out testing  
-        # setting the train, validation and test indecies
+        # 1 setting the train, validation and test indecies
         validation_ratio = float(self.dataset_config['val_share'])
         test_ratio = float(self.dataset_config['test_share'])
         self.set_seed()
-        self.idx_train, rest_data = train_test_split(self.idx_label, test_size=validation_ratio+test_ratio)
+        self.idx_train, rest_data = train_test_split(self.idx_abs, test_size=validation_ratio+test_ratio)
         self.set_seed()
         self.idx_test, self.idx_val = train_test_split(rest_data, test_size=validation_ratio/(test_ratio+validation_ratio))
+
+
+        # setting the labeled indecies
+        initial_lb_size = float(self.dataset_config['labeled_share']) * len(self.idx_train) # initial labeled size
+        self.set_seed()
+        self.idx_label = np.random.choice(self.idx_train, size=math.floor(initial_lb_size), replace=False) # original labeled indecies
+
+        self.get_unlabeled_indecies()
+
+        print(f"Dataset: {len(self.dataset)}")
+        print(f"Train: {len(self.idx_train)}")
+        print(f"Validation: {len(self.idx_val)}")
+        print(f"Test: {len(self.idx_test)}")
+        print(f"Initial labeled data: {len(self.idx_label)}")
+
+        
 
     def set_seed(self, seed: Optional[int] = None) -> None:
         if seed is None:
@@ -47,7 +60,7 @@ class Pool():
     
     def get_loaders(self) -> tuple:
         '''Returns the train, validation and test loaders respectively.'''
-        train_loader = DataLoader(Subset(self.dataset, self.idx_train), batch_size=int(self.dataset_config['batch_size']), shuffle=True)
+        train_loader = DataLoader(Subset(self.dataset, self.idx_label), batch_size=int(self.dataset_config['batch_size']), shuffle=True)
         val_loader = DataLoader(Subset(self.dataset, self.idx_val), batch_size=int(self.dataset_config['batch_size']), shuffle=False)
         test_loader = DataLoader(Subset(self.dataset, self.idx_test), batch_size=int(self.dataset_config['batch_size']), shuffle=False)
         return train_loader, val_loader, test_loader
@@ -55,15 +68,15 @@ class Pool():
     def get_test_loaders(self) -> tuple:
         '''Returns the train and test loaders respectively. train is consisted of the union train and validation sets.'''
         train_loader = DataLoader(Subset(self.dataset, np.concatenate((
-            self.idx_train, self.idx_val))), batch_size=int(self.dataset_config['batch_size']), shuffle=True)
+            self.idx_label, self.idx_val))), batch_size=int(self.dataset_config['batch_size']), shuffle=True)
         test_loader = DataLoader(Subset(self.dataset, self.idx_test), batch_size=int(self.dataset_config['batch_size']), shuffle=False)
         return train_loader, test_loader
     
     def add_labeled_data(self, idx: int) -> None:
         '''Add labeled data to the pool.'''
         self.idx_label = np.concatenate((self.idx_label, [idx]))
-        self.idx_train = np.concatenate((self.idx_train, [idx]))
+        #self.idx_train = np.concatenate((self.idx_train, [idx]))
 
     def get_unlabeled_indecies(self) -> np.ndarray:
         '''Returns the unlabeled data.'''
-        return np.setdiff1d(self.idx_abs, self.idx_label)
+        return np.setdiff1d(self.idx_train, self.idx_label)
