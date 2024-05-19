79fb8c8c118a097ff6ea812895984bc4e848ed61
Amir Hossein Eyvazkhani <amirhossein_eyvazkhani@yahoo.com>
2024-05-19 11:05:27 +0200

    fix exception handling

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     parser.add_argument('--d', type=str, help='Dataset name')
     parser.add_argument('--r', type=int, help='random seed')
     args = parser.parse_args()
+    al = ActiveLearning(dataset_name=args.d, random_seed=args.r)
     # start Active Learning! :)
     try:
-        al = ActiveLearning(dataset_name=args.d, random_seed=args.r)
         al.run()
     except Exception as e:
         file_path = al.data_logger.log_primary_results(al.test_loss_list, al.best_dropout_rate_list, al.best_l2_reg_list, al.test_accuracy_list)
