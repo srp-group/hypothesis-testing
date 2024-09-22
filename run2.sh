aa9ead7f795c4baf8649df06760dd455e917cbc3
Amir Hossein Eyvazkhani <amirhossein_eyvazkhani@yahoo.com>
2024-09-22 15:07:44 +0200

    test run

---
+++ run2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=SRP2
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=eyvazkhani@uni-hildesheim.de
+#SBATCH --mail-type=ALL
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+echo "This is a test echo"
+source activate SRP_ENV
