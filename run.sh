201136b350a6d52aba441947c9f47fd7a50f536f
Amir Hossein Eyvazkhani <amirhossein_eyvazkhani@yahoo.com>
2024-09-25 19:54:11 +0200

    1

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=SRP
-#SBATCH --output=../out/%Y-%m-%d_%H-%M-%S/%x_%j.log
-#SBATCH --error=../out/%Y-%m-%d_%H-%M-%S/%x_%j.err
+#SBATCH --output=../out/%x_%j.log
+#SBATCH --error=../out/%x_%j.err
 #SBATCH --mail-user=eyvazkhani@uni-hildesheim.de
 #SBATCH --mail-type=ALL
 #SBATCH --partition=STUD
 #SBATCH --gres=gpu:1
 
-source activate SRP_ENV
-srun python main.py --r 42 --d "dna" --a "random" --m "MLP"
+echo "Starting job"
+# source activate SRP_ENV
+# srun python main.py --r 42 --d "dna" --a "random" --m "MLP"
