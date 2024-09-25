e12558082f4f86d04beac0466c92e5a98a6b9998
Amir Hossein Eyvazkhani <amirhossein_eyvazkhani@yahoo.com>
2024-09-25 20:02:08 +0200

    2

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ if __name__ == '__main__':
         al.run()
     except Exception as e:
         file_path = al.data_logger.log_primary_results(al.test_loss_list, al.best_dropout_rate_list, al.best_l2_reg_list, al.test_accuracy_list)
-        al.visualizer.plot_primary_results(al.test_loss_list, al.best_dropout_rate_list, al.best_l2_reg_list, al.test_accuracy_list)
         al.log_params()
-        al.visualizer.plot_results(file_path)
         print("An error occurred! but the results are saved!")
         print(e)
