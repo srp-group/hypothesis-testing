c7d355162cd8f06a5ff2a231ef428c902301429d
Amir Hossein Eyvazkhani <amirhossein_eyvazkhani@yahoo.com>
2024-09-25 19:38:37 +0200

    1

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+
+# Get the current date and time in the format YYYY-MM-DD_HH-MM-SS
+current_date_time=$(date +"%Y-%m-%d_%H-%M-%S")
 #SBATCH --job-name=SRP
-#SBATCH --output=../out/%x_%j.log
-#SBATCH --error=../out/%x_%j.err
+#SBATCH --output=../out/$current_date_time/%x_%j.log
+#SBATCH --error=../out/$current_date_time/%x_%j.err
 #SBATCH --mail-user=eyvazkhani@uni-hildesheim.de
 #SBATCH --mail-type=ALL
 #SBATCH --partition=STUD
